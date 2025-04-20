2798205d62c271a1a4f88490eca1ec9355fde92f
Madro014 <spinnercrack1@gmail.com>
2025-04-20 13:20:44 -0500

    Actualización del footer y configuración de vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/proyecto_empresarial/', // nombre del repositorio en GitHub
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/proyecto_empresarial/', // Use root path for dev, repo name for production
+  }
+  
+  return config
 })
